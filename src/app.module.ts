c0220c6cd35085e6e3689dc3970969190fbedbb8
Bohdan <bohdanmatv1@gmail.com>
2024-04-09 14:26:04 +0300

    feature-mango atlas db connection implememnted

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@basic-db.kbspaui.mongodb.net/`,
+    ),
   ],
   controllers: [AppController],
   providers: [AppService],
